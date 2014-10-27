024a897ef863b363c6490ee59e077996502bf503
njdup <njdupp@gmail.com>
2014-10-27 16:13:31 -0700

    Fixed error in logic when checking if keyword sub mapping empty

--- common/djangoapps/util/keyword_substitution.py
+++ common/djangoapps/util/keyword_substitution.py
@@ -29,7 +29,7 @@ def keyword_function_map_is_empty():
     """
     Checks if the keyword function map has been filled
     """
-    return bool(KEYWORD_FUNCTION_MAP)
+    return not bool(KEYWORD_FUNCTION_MAP)
 
 def add_keyword_function_map(mapping):
     """
